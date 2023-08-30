232acef02b1d6dab3013292e23066b4db97d5311
Mailson Almeida Cerqueira <124589889+Mailsonalmeida05@users.noreply.github.com>
2023-08-30 15:02:13 -0300

    commit init

---
+++ pasteis.js
@@ -0,0 +1,11 @@
+let menuJson = [
+    {id:1, name:'PRODUTO R$ 9,90', img:'icone_boliviano.svg', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'Descrição do Produto.'},
+    {id:2, name:'PRODUTO R$ 9,90', img:'icone_bolinhocalabresa.svg', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'Descrição do Produto.'},
+    {id:3, name:'PRODUTO R$ 12,00', img:'icone_risole.svg', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'Descrição do Produto. '},
+    {id:4, name:'PRODUTO R$ 8888,00', img:'icone_enroladinho.svg', price:[8.00, 8.00, 8.00], sizes:[' ', '',''], description:'Descrição do Produto. '},
+    {id:5, name:'PRODUTO R$ 8,90', img:'icone_coxinha.svg', price:[8.90, 8.90, 8.90], sizes:[' ', '',''], description:'Descrição do Produto.'},
+    {id:6, name:'PRODUTO R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Descrição do Produto. '},
+   
+ 
+   
+];
